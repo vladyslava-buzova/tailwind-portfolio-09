7c7646161c52f71a3aba50bcf85f91307d2ad120
Vladyslava Buzova <dev.vladyslava@gmail.com>
2023-10-20 13:41:22 -0600

    colors

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ import SectionTitle from "./SectionTitle"
 const Projects = () => {
   return (
     <section className="text-black py-20 align-elements" id='projects'>
-      <SectionTitle text='web creations' />
+      <SectionTitle text='Projects' />
       <div className="py-16 grid lg:grid-cols-2 lx:grid-cols-3 gap-8">
         {projects.map((project) => {
           return (
